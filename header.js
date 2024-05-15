7c5a1b1857350f45c654891774e76c20db3f8bb3
Satyam Rajput <satyamrajput081998@gmail.com>
2024-05-15 06:07:06 -0400

    setup done with Linkedin

--- header.js
+++ header.js
@@ -7,7 +7,7 @@ export class Portfolio{
         <div class ="left"><h2 class ="sat">SATYAM </h2>
         <h2 class="raj"> RAJPUT</h2>
         </div>
-        <a href="#" id="menu-icon"><i style="font-size:35px" class="fa">&#xf0c9;</i></a>
+        <a href="#id" id="menu-icon"><i style="font-size:35px" class="fa">&#xf0c9;</i></a>
         <div class ="middle"> 
         <a href="#" class="close"></a>
         <li><a href = "#homeid"> Home </a></li>
@@ -18,7 +18,7 @@ export class Portfolio{
         </div>
         <div class ="right">
         <div>|</div>
-        <a href ="https://www.linkedin.com/in/satyam-rajput-0457a11b4/" target="_blank" v><i class="fa fa-linkedin" aria-hidden="true" style="font-size:20px;"></i></a>
+        <a href ="https://www.linkedin.com/in/satyam-rajput-16b999301/" target="_blank" v><i class="fa fa-linkedin" aria-hidden="true" style="font-size:20px;"></i></a>
         <a href ="https://github.com/SATYAM08RAJPUT" target="_blank" class ="icons"><i class="fa fa-github" aria-hidden="true" style="font-size:20px;"></i></a>
         </div>
         </div>
